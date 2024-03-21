48f7a95d6b63ca439f521069256cc0887bf72aeb
hyunseok <hyun104467@naver.com>
2024-03-21 19:27:27 +0900

    hotfix: 머지 오류 해결

--- src/main/java/goorm/brainsnack/quiz/service/QuizService.java
+++ src/main/java/goorm/brainsnack/quiz/service/QuizService.java
@@ -6,8 +6,6 @@ import static goorm.brainsnack.quiz.dto.QuizRequestDto.*;
 import static goorm.brainsnack.quiz.dto.QuizResponseDto.*;
 
 public interface QuizService {
-    QuizResponseDto.QuizDto findQuiz(Long quizId);
-    QuizResponseDto.GetTotalMemberDto getTotalNum(Long memberId);
     QuizResponseDto.QuizDetailDto findQuiz(Long quizId);
     GetTotalMemberDto getTotalNum(Long memberId);
     CategoryQuizListDto getCategoryQuizzes(String category);
