3d0f5925536317c65ab63fe2fd99d64c2f19ae18
Hyunstone <hyun104467@naver.com>
2024-03-23 22:16:40 +0900

    hotfix: 배포용 임시 작업

--- src/test/java/com/goorm/brainsnack/BrainsnackApplicationTests.java
+++ src/test/java/com/goorm/brainsnack/BrainsnackApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BrainsnackApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
